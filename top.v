f99c36061dda830a681010a1fff0481dead1d632
lovexyn0827 <2717498529@qq.com>
2025-05-26 13:59:19 +0800

    Avoid using  in procedures

--- top.v
+++ top.v
@@ -13,9 +13,41 @@ assign RD1 = regs[RI1];
 assign RD2 = regs[RI2];
 
 always @(posedge CLK) begin
-    if (RST)
-        for (integer i = 0; i < 32; i = i + 1)
-            regs[i] <= 0;
+    if (RST) begin
+        regs[1] <= 32'b0;
+        regs[2] <= 32'b0;
+        regs[3] <= 32'b0;
+        regs[4] <= 32'b0;
+        regs[5] <= 32'b0;
+        regs[6] <= 32'b0;
+        regs[7] <= 32'b0;
+        regs[8] <= 32'b0;
+        regs[9] <= 32'b0;
+        regs[10] <= 32'b0;
+        regs[11] <= 32'b0;
+        regs[12] <= 32'b0;
+        regs[13] <= 32'b0;
+        regs[14] <= 32'b0;
+        regs[15] <= 32'b0;
+        regs[16] <= 32'b0;
+        regs[17] <= 32'b0;
+        regs[18] <= 32'b0;
+        regs[19] <= 32'b0;
+        regs[20] <= 32'b0;
+        regs[21] <= 32'b0;
+        regs[22] <= 32'b0;
+        regs[23] <= 32'b0;
+        regs[24] <= 32'b0;
+        regs[25] <= 32'b0;
+        regs[26] <= 32'b0;
+        regs[27] <= 32'b0;
+        regs[28] <= 32'b0;
+        regs[29] <= 32'b0;
+        regs[30] <= 32'b0;
+        regs[31] <= 32'b0;
+        /*for (integer i = 0; i < 32; i = i + 1)
+            regs[i] <= 0;*/
+    end
     else if (WRT & WI != 5'b0)
         regs[WI] <= WD;
 end
@@ -515,8 +547,9 @@ reg[31:0] mem[255:0];
 
 always @(posedge CLK) begin
     if (RST)
-        for (integer i = 0; i < 256; i = i + 1)
-            mem[i] <= 0;
+        /*for (integer i = 0; i < 256; i = i + 1)
+            mem[i] <= 0;*/
+        mem[0] <= 32'b0;
     else if (MWRT)
         mem[ADDR[9:2]] <= DAT;
 end
